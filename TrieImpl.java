b050025426a6e23e7b7591466e985bb2e52bf597
BC <chen.b.brian@gmail.com>
2017-11-12 03:43:40 -0500

    Trie Fix

--- TrieImpl.java
+++ TrieImpl.java
@@ -1,3 +1,7 @@
+package app;
+
+import java.util.HashMap;
+
 class TrieNode {
     Character c;
     Boolean isLeaf = false;
@@ -8,72 +12,50 @@ class TrieNode {
     }
 }
 
-class Trie {
-    private TrieNode root;
-    
-    public Trie() {this.root = new TrieNode();}
+public class Trie {
+    private TrieNode root = new TrieNode();
+
     public void insertWord(String word) {
-        if(word == null || word.length() == 0) {
+        if (word == null || word.length() == 0) {
             return;
         }
         TrieNode cur = this.root;
-        for(char c : word.toCharArray())
-        {
-            if(cur.children.containsKey(c)) {
-                cur = cur.children.get(c);
+        for (char c : word.toCharArray()) {
+            // .containsKey method of Map is good, but when you are going to call .get after calling
+            // the .containsKey, and if you don't care about difference between absent and null values,
+            // it will be a bit faster (and cleaner) not to use contains at all.
+            TrieNode next = cur.children.get(c);
+            if (next == null) {
+                next = new TrieNode();
+                cur.children.put(c, next);
             }
-            else {
-                TrieNode letter = new TrieNode(c);
-                cur.children.put(c, letter);
-                cur = cur.children.get(c);
-            }
-            
+            cur = next;
         }
-        
-        if(cur.children.isEmpty())
-        {
-            cur.isLeaf = true;
-        }
+        cur.isLeaf = true;
     }
-    public Boolean searchWord(String word) {
+
+    public boolean searchWord(String word) {
+        TrieNode node = getNodeByPath(word);
+        return node != null && node.isLeaf;
+    }
+
+    // Boolean can contain 3 values: true, false and null.
+    // When you are not going to use null, it is usually better to use boolean instead.
+    public boolean searchPrefix(String word) {
+        TrieNode node = getNodeByPath(word);
+        return node != null && node.children.size() > 0;
+    }
+
+    // You have had the same code with the same meaning in 2 methods.
+    // It is usually better to move duplicate code to separate method.
+    private TrieNode getNodeByPath(String word) {
         TrieNode cur = this.root;
-        for(char c : word.toCharArray())
-        {
-        
-            if(cur.children.containsKey(c))
-            {
-                cur = cur.children.get(c);
+        for (char c : word.toCharArray()) {
+            cur = cur.children.get(c);
+            if (cur == null) {
+                return null;
             }
-            else {
-                return false;
-            }
-
         }
-        
-        return true;
-
-
+        return cur;
     }
-    public Boolean searchPrefix(String word) {
-        TrieNode cur = this.root;
-        for(char c : word.toCharArray())
-        {
-            if(cur.children.containsKey(c))
-            {
-                cur = cur.children.get(c);
-            }
-            else {
-                return false;
-            }
-
-        }
-        
-        if(cur.isLeaf) {
-            return false;
-        }
-        
-        return true;
-    }
-    
-    
 }
